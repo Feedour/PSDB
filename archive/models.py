4c08f5bb782ece2c98c75a2613a9e1762e4db699
Trostnick <trostnick97@mail.ru>
2017-02-19 16:52:14 +0300

    archive modules

--- archive/models.py
+++ archive/models.py
@@ -3,6 +3,42 @@ from django.db import models
 from django.utils import timezone
 from PIL import Image
 
+
+class BG(models.Model):
+    name = models.CharField(max_length=200)
+    info = models.TextField()
+    st_nab = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to='images/BG/', blank=True)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class Sotrudnik(models.Model):
+    name = models.CharField(max_length=100)
+    info = models.TextField()
+    image = models.ImageField(upload_to='images/Sotrudnik/', blank=True)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
 class Planet(models.Model):
     name = models.CharField(max_length=100)
     history = models.TextField()
@@ -20,44 +56,7 @@ class Planet(models.Model):
         return self.name
 
 
-class Person(models.Model):
-    name = models.CharField(max_length=100)
-    info = models.TextField()
-    image = models.ImageField(upload_to='static/images/Person/', blank=True)
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-    planet = models.ForeignKey(Planet, null=True, on_delete=models.SET_NULL)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.name
-
-
-class BG(models.Model):
-    name = models.CharField(max_length=200)
-    info = models.TextField()
-    st_nab = models.CharField(max_length=100, null=True)
-    image = models.ImageField(upload_to='static/images/BG/', blank=True)
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-    last_st_nab = models.ForeignKey(Person, null=True, on_delete=models.SET_NULL)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.name
-
-
-class Mission(models.Model):
+class Visadki(models.Model):
     name = models.CharField(max_length=50)
     target = models.TextField()
     history = models.TextField()
@@ -66,13 +65,10 @@ class Mission(models.Model):
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
-    planet = models.ForeignKey(Planet, null=True, on_delete=models.SET_NULL)
-    bg = models.ForeignKey(BG, null=True, on_delete=models.SET_NULL)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.name
-
+        return self.title
