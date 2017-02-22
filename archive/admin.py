ed9197e0bc0f08ea4c726240d197e8b4d187e8a8
Trostnick <trostnick97@mail.ru>
2017-02-22 12:31:02 +0300

    Merge branch 'dev' of https://github.com/Feedour/PSDB into dev
    # Conflicts:
    #	archive/models.py

--- archive/admin.py
+++ archive/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BG
+from .models import Planet
+from .models import Person
+from .models import Mission
+
+admin.site.register(BG)
+admin.site.register(Planet)
+admin.site.register(Person)
+admin.site.register(Mission)
 
 # Register your models here.
--- archive/models.py
+++ archive/models.py
@@ -3,17 +3,14 @@ from django.db import models
 from django.utils import timezone
 from PIL import Image
 
-
-class BG(models.Model):
-    name = models.CharField(max_length=200)
-    info = models.TextField()
-    st_nab = models.CharField(max_length=100, null=True)
-    image = models.ImageField(upload_to='images/BG/', blank=True)
+class Planet(models.Model):
+    name = models.CharField(max_length=100)
+    history = models.TextField()
+    image = models.ImageField(upload_to='images/Planet/', blank=True)
     created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
-    last_st_nab = models.ForeignKey(Person, null=True, on_delete=models.SET_NULL)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -41,23 +38,6 @@ class Person(models.Model):
         return self.name
 
 
-class Planet(models.Model):
-    name = models.CharField(max_length=100)
-    history = models.TextField()
-    image = models.ImageField(upload_to='images/Planet/', blank=True)
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.name
-
-
 class Mission(models.Model):
     name = models.CharField(max_length=50)
     target = models.TextField()
@@ -69,6 +49,25 @@ class Mission(models.Model):
         blank=True, null=True)
     planet = models.ForeignKey(Planet, null=True, on_delete=models.SET_NULL)
     bg = models.ForeignKey(Planet, null=True, on_delete=models.SET_NULL)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class BG(models.Model):
+    name = models.CharField(max_length=200)
+    info = models.TextField()
+    st_nab = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to='images/BG/', blank=True)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+    last_st_nab = models.ForeignKey(Person, null=True, on_delete=models.SET_NULL)
 
     def publish(self):
         self.published_date = timezone.now()
