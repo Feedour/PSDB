8f900eec475fd9c05a677d2e5a546bed6bbc8c82
meeg <golovay14@gmail.com>
2017-02-22 13:59:31 +0300

    Сделана миграция для моделей обоих приложений

--- PSDB/settings.py
+++ PSDB/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'archive.apps.ArchiveConfig',
+    'shedule.apps.SheduleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
