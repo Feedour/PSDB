ad43d593c04a2a02013c20aeeddee001a142cb8c
Your Name <you@example.com>
2017-03-06 20:24:39 +0300

    Merge branch 'dev' of https://github.com/Feedour/PSDB into dev
    # Conflicts:
    #	archive/models.py

--- archive/urls.py
+++ archive/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='archive'),
-    url(r'^person/(?P<pk>[0-9]+)/$', views.person, name='person'),
-    url(r'^bg/(?P<pk>[0-9]+)/$', views.bg, name='bg'),
-    url(r'^planet/(?P<pk>[0-9]+)/$', views.planet, name='planet'),
-    url(r'^mission/(?P<pk>[0-9]+)/$', views.mission, name='mission'),
+    url(r'^/person/(?P<pk>[0-9]+)/$', views.person, name='person'),
+    url(r'^/bg/(?P<pk>[0-9]+)/$', views.bg, name='bg'),
+    url(r'^/planet/(?P<pk>[0-9]+)/$', views.planet, name='planet'),
+    url(r'^/mission/(?P<pk>[0-9]+)/$', views.mission, name='mission'),
     ]
