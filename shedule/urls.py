a60d4b84e8513f9d99cc301b2abc90a6f9bdb820
meeg <golovay14@gmail.com>
2017-03-12 01:42:25 +0300

    Добавлена страница логина пользователся. Добавлен временный костыль для обработки анонимного поьзователя.

--- shedule/urls.py
+++ shedule/urls.py
@@ -5,6 +5,8 @@ app_name = 'shedule'
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^login/$', views.enter, name = 'login'),
+    url(r'^exit/$', views.exit, name = 'logout'),
     url(r'^(?P<day>[0-9]+)/$', views.shedule, name='shedule'),
     url(r'^(?P<day>[0-9]+)/(?P<pk>[0-9]+)/$', views.lesson, name='lesson'),
     url(r'^(?P<day>[0-9]+)/(?P<pk>[0-9]+)/enroll/$', views.enroll, name='enroll'),
--- shedule/views.py
+++ shedule/views.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, reverse
 from .models import Lesson, Watcher
 from django.views import generic
-from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth import authenticate, logout, login
 
 
 def index(request):
@@ -16,7 +16,10 @@ def shedule(request, day):
     return HttpResponseRedirect(reverse('shedule:lesson', args=(1,0)))
 
 def lesson (request, day, pk):
-    watcher = request.user.watcher
+    if (request.user.is_authenticated):
+        watcher = request.user.watcher
+    else:
+        watcher = Watcher(name='Аноним', pk=0)
     day_list = Lesson.objects.fake_day_list()
     lesson_list = Lesson.objects.day_lessons(day)
     current_lesson = lesson_list.filter(pk=pk).first()
@@ -26,7 +29,7 @@ def lesson (request, day, pk):
                'current_lesson': current_lesson,}
     return render(request, 'shedule/shedule.html', context)
 
-def enroll(request, day, pk):
+def enroll(request, day,pk):
     lesson = get_object_or_404(Lesson, pk=pk)
     watcher = request.user.watcher
     students = lesson.students.all();
@@ -47,7 +50,26 @@ def enroll(request, day, pk):
             messages.success(request, "Готово! Вы записались на это занятие")
     else:
         messages.error(request, 'Вы не можете записаться на этот МК')
-    return HttpResponseRedirect(reverse('shedule:shedule', args=(pk,)))
+    return HttpResponseRedirect(reverse('shedule:lesson', args=(lesson.day,pk,)))
+
+def exit(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('shedule:login'))
+
+def enter(request):
+    if (not request.POST):
+        return render(request, 'shedule/signin.html')
+    else:
+        username = request.POST['login']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('shedule:lesson', args=(1, 0)))
+        else:
+             messages.error(request, 'Неверный логин/пароль')
+             return render(request, 'shedule/signin.html')
+
 
 
 class DetailWatcher(generic.DetailView):
