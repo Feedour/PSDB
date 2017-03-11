a2396e1eb0dd749889e211baff2afdf993cb602a
Your Name <you@example.com>
2017-03-11 22:31:10 +0300

    теперь при неправильном номере записи открывает первую ссылку в данной категории

--- archive/views.py
+++ archive/views.py
@@ -5,7 +5,7 @@ from .models import BG
 from .models import Planet
 from .models import Person
 from .models import Mission
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, reverse
 
 
 def index(request):
@@ -14,27 +14,43 @@ def index(request):
 
 def person(request,pk):
     person_list=Person.objects.all();
+    if person_list.filter(pk=pk):
+        current_person = Person.objects.get(pk=pk)
+    else:
+        current_person = person_list.first()
     context = {'Person_list':person_list,
-               'current_person':Person.objects.get(pk=pk)}
+               'current_person':current_person}
     return render(request, 'archive/people.html', context)
 
 
 def bg(request, pk):
     bg_list = BG.objects.all();
+    if bg_list.filter(pk=pk):
+        current_bg = BG.objects.get(pk=pk)
+    else:
+        current_bg = bg_list.first()
     context = {'BG_list': bg_list,
-               'current_bg': BG.objects.get(pk=pk)}
+               'current_bg': current_bg}
     return render(request, 'archive/bg.html', context)
 
 
 def planet(request,pk):
     planet_list=Planet.objects.all();
+    if planet_list.filter(pk=pk):
+        current_planet=Planet.objects.get(pk=pk)
+    else:
+        current_planet=planet_list.first()
     context = {'Planet_list': planet_list,
-               'current_planet': Planet.objects.get(pk=pk)}
+               'current_planet': current_planet}
     return render(request, 'archive/planet.html', context)
 
 
 def mission(request,pk):
     mission_list=Mission.objects.all();
+    if mission_list.filter(pk=pk):
+        current_mission=Mission.objects.get(pk=pk)
+    else:
+        current_mission=mission_list.first()
     context = {'Mission_list': mission_list,
-               'current_mission': Mission.objects.get(pk=pk)}
+               'current_mission': current_mission}
     return render(request, 'archive/mission.html', context)
